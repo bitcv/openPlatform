30b26311e0bba2fac417b76f0ac2b101403a76a4
Korey <wang746326947@qq.com>
2018-10-31 17:33:02 +0800

    update cli

--- JSSDK/config/webpack.dev.conf.js
+++ JSSDK/config/webpack.dev.conf.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: 'bcvwallet.js',
+    filename: 'bcvwallet.min.js',
     library: 'bcvWallet',
     libraryTarget: 'umd'
   },
