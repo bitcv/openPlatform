41fd557eabd6a483533999a00b2b41141757ed32
xiaofei <zhangxiaofei1001@gmail.com>
2018-11-13 17:38:59 +0800

    update

--- BE-DEMO/php/OpenPlatform.Config.php
+++ BE-DEMO/php/OpenPlatform.Config.php
@@ -11,8 +11,8 @@ class OpenPlatformConfig
      * APPSECRET : 开放平台提供的 APPSECRET
      * REDIRECTURL : 授权后重定向的回调地址，使用 urlencode 对链接进行编码
      * */
-    const APPID = 'b42126b2ed59f2cb';
-    const APPSECRET = 'E48WxAXR0fqemw3uIVuVmauGCHjpgldn';
+    const APPID = '';
+    const APPSECRET = '';
     const REDIRECTURL = 'http://sdkdemo.bitcv.com';
     const DOMAIN = 'http://open.bitcv.com';
 }
